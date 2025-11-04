8284df02579d329ba4f0d2913c5103f5cd9a0b5b
Grenish rai <mrcoder2033d@gmail.com>
2025-11-04 15:24:11 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,5 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
-    </div>
-  );
+  return <Spinner />;
 }
